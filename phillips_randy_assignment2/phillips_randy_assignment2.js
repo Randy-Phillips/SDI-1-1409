535a52a360d64eac88dcb13e52ed2e0da2b36201
Randy-Phillips <attackruby@Randys-MacBook-Pro.local>
2014-09-11 10:09:00 -0400

    Beginning - Procedure
    Edited HTML to work with new .js name.
    Added Variables with placeholder names.
    Procedure function added and called with prompt called

--- phillips_randy_assignment2/phillips_randy_assignment2.js
+++ phillips_randy_assignment2/phillips_randy_assignment2.js
@@ -1,1 +1,41 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
+
+
+
+// MY VARIABLES
+
+var myProfessor = "Lee";
+var mNumber = 100;
+var myBoolean = true;
+
+
+// MY FUNCTIONS
+		// PROCEDURE
+		
+function openMessage(myProfessor) {
+	
+	if (myProfessor === "Lee") {
+		console.log("You are my Professor");
+	} else {
+		console.log("You are NOT my Professor");
+	}
+}
+
+
+
+
+
+// MAIN CODE
+
+	// MY PROMPT
+myProfessor = prompt("What is your name, Professor?");
+	// PROCEDURE FUNCTION
+openMessage(myProfessor);
+	
+
+
+
+
+
+
+
