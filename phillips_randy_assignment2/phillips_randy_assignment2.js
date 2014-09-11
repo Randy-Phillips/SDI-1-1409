dd1487031d856eb8252507c3a68c740ebe7be756
Randy-Phillips <attackruby@Randys-MacBook-Pro.local>
2014-09-11 10:48:16 -0400

    Number Function
    Working on Number Function.
    Counting from myNum (number imputed by user) to 10, either up or down,
    depending on number imputed.

--- phillips_randy_assignment2/phillips_randy_assignment2.js
+++ phillips_randy_assignment2/phillips_randy_assignment2.js
@@ -5,7 +5,7 @@
 // MY VARIABLES
 
 var myProfessor = "Lee";
-var mNumber = 100;
+var mNum;
 var myBoolean = true;
 
 
@@ -22,20 +22,28 @@ function openMessage(myProfessor) {
 }
 
 
+function myMathSkills(myNum) { 
+	 
+	console.log("Today, " + myProfessor + ", we will be count to 10, from your lucky number, " + myNum);
+	console.log("newNum var: " + newNum);
+	if (myNum < 10) {
+		console.log("We are starting at " + myNum + " and counting up to 10");
+		for (myNum < 10; myNum++) {
+			console.log("We are at " + myNum + ", keep counting!");
+		}
+	}
+	
+}
 
 
 
 // MAIN CODE
 
-	// MY PROMPT
+
 myProfessor = prompt("What is your name, Professor?");
-	// PROCEDURE FUNCTION
 openMessage(myProfessor);
 	
-
-
-
-
-
+myNum = prompt("what is your lucky number, " + myProfessor + "?","1-50");
+myMathSkills(myNum);
 
 
