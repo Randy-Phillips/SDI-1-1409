778c7c9db71749a68f5f409cec5dc4db6c4d9dfa
Randy-Phillips <attackruby@Randys-MacBook-Pro.local>
2014-09-18 16:09:39 -0400

    console log shorthand variables and more
    added more global variables and nested loop function for starting
    message

--- phillips_randy_assignment3/phillips_randy_assignment3.js
+++ phillips_randy_assignment3/phillips_randy_assignment3.js
@@ -2,15 +2,24 @@
 
 // Global Variables
 
-var annualSalary = 1;
 var avgHourYr = 2080;
+var firstIncome = 1;
+var secondIncome = 2;
+var thirdIncome = 3;
+var getReady = false;
+
+// Console.log Shorthand
+function say(say) {
+	console.log(say);
+}
+
 
 
 // My Procedure
 
 function newFuction () {
 	
-	console.log("So, three people, three different hourly earnings, three different tax brackets.. how much do they make a year!");
+	say("So, three people, three different hourly earnings, three different tax brackets.. how much do they make a year!");
 	
 }
 // For Loop 
@@ -19,19 +28,43 @@ var forLoop = function() {
 	
 	for (var firstKey in jsonData.playerNames) {
 		
-		console.log("________________");
+		say("________________");
 		
-		console.log(jsonData.playerNames[firstKey]);
+		say(jsonData.playerNames[firstKey]);
 	}
 }
 
-// 
+
+// While Loop + Nested Loop + Function
+
+var readyLoop = function() {
+	var getReady = confirm("Are you ready to see their annual incomes?");
+	
+	while (getReady === false) {
+		
+		var getReady = confirm("Are you ready to see their annual incomes?");
+	
+	}
+
+	say("Lets get a count down going!");
+
+	for(i = 3; i > 0 ; i--) {
+
+		say(i);
+	}
+	say("lets go!");
+}
+
+
+
+
+// Returned Number(s) + 1 Function + Local Variables
 
 var annualIncome = function (personName,personIncome,personTax) {
 	
 	var salaryCalc = (personIncome * (1 - personTax)) * avgHourYr;
 	
-	console.log(personName + " makes $" + salaryCalc  + " a year");
+	say(personName + " makes $" + salaryCalc  + " a year");
    
    return salaryCalc;
 }
@@ -39,18 +72,18 @@ var annualIncome = function (personName,personIncome,personTax) {
 
 
 // Main Code
-
+readyLoop();
 newFuction();
 forLoop();
 
 var firstIncome = annualIncome(jsonData.playerNames[0].personFirstName,jsonData.playerNames[0].hrIncome,jsonData.playerNames[0].overallTax);
 
-annualIncome(jsonData.playerNames[1].personFirstName,jsonData.playerNames[1].hrIncome,jsonData.playerNames[1].overallTax);
+var secondIncome = annualIncome(jsonData.playerNames[1].personFirstName,jsonData.playerNames[1].hrIncome,jsonData.playerNames[1].overallTax);
 
-annualIncome(jsonData.playerNames[2].personFirstName,jsonData.playerNames[2].hrIncome,jsonData.playerNames[2].overallTax);
-
-
+var thridIncome = annualIncome(jsonData.playerNames[2].personFirstName,jsonData.playerNames[2].hrIncome,jsonData.playerNames[2].overallTax);
 
 
-console.log(firstIncome);
+
+
+say(firstIncome);
 //*/
