81f7d76f5f9fb3e425fa0cd5aad8b9bc120d11a6
Randy-Phillips <attackruby@Randys-MacBook-Pro.local>
2014-09-18 13:38:23 -0400

    JsonData Added with test scripts
    JsonData added with test scripts

---
+++ phillips_randy_assignment3/json.js
@@ -0,0 +1,26 @@
+var jsonData = {
+	
+	"playerNames":
+		[
+			{
+				"personFirstName"   : "Randy",
+				"personLastName"    : "Phillips",
+				"personHrIncome"    :  10,
+				"overallTax"        : .20,
+			},
+			{
+				"personFirstName"   : "John",
+				"personLastName"    : "Doe",
+				"personHrIncome"    :  12,
+				"overallTax"        : .22,
+			},
+			{
+				"personFirstName"   : "Lee",
+				"personLastName"    : "Lewis",
+				"personHrIncome"    :  14,
+				"overallTax"        : .25,
+			},
+		
+		
+		]
+}
--- phillips_randy_assignment3/phillips_randy_assignment3.js
+++ phillips_randy_assignment3/phillips_randy_assignment3.js
@@ -1,1 +1,31 @@
-alert("JavaScript works!");
+//*
+
+console.log("it works");
+
+
+// Global Variables
+
+var anualSalary = 1;
+var avgHourYr = 2080;
+
+
+
+// Nested Loop 
+
+var nestedLoop = function() {
+	
+	for (var firstKey in jsonData.playerNames) {
+		console.log("________________");
+		for (var secondKey in jsonData.playerNames[firstKey]) {
+			
+			console.log(jsonData.playerNames[firstKey][secondKey]);
+			
+		}
+	}
+}
+
+
+
+nestedLoop();
+
+// console.log(jsonData.playerNames[0]);
