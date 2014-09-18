656ab34e8a7a9f671872e439e6243aed5966e9cd
Randy-Phillips <attackruby@Randys-MacBook-Pro.local>
2014-09-18 16:55:09 -0400

    Added array argument

--- phillips_randy_assignment3/json.js
+++ phillips_randy_assignment3/json.js
@@ -24,3 +24,5 @@ var jsonData = {
 		
 		]
 }
+
+
--- phillips_randy_assignment3/phillips_randy_assignment3.js
+++ phillips_randy_assignment3/phillips_randy_assignment3.js
@@ -1,4 +1,3 @@
-//*
 
 // Global Variables
 
@@ -7,6 +6,7 @@ var firstIncome = 1;
 var secondIncome = 2;
 var thirdIncome = 3;
 var getReady = false;
+var array = [10,12,15,22,92,41];
 
 // Console.log Shorthand
 function say(say) {
@@ -22,6 +22,8 @@ function newFuction () {
 	say("So, three people, three different hourly earnings, three different tax brackets.. how much do they make a year!");
 	
 }
+
+
 // For Loop 
 
 var forLoop = function() {
@@ -43,19 +45,17 @@ var readyLoop = function() {
 	while (getReady === false) {
 		
 		var getReady = confirm("Are you ready to see their annual incomes?");
-	
 	}
-
+	
 	say("Lets get a count down going!");
 
 	for(i = 3; i > 0 ; i--) {
-
 		say(i);
 	}
 	say("lets go!");
+	
+	return getReady;
 }
-
-
 
 
 // Returned Number(s) + 1 Function + Local Variables
@@ -71,8 +71,23 @@ var annualIncome = function (personName,personIncome,personTax) {
 
 
 
+// Array function + number argument + string argument
+
+var Array2 = function(number,string) {
+	array[array.length] = number;
+	array[array.length] = string;
+	return array;
+	
+}
+
+// Array argument
+
+var arrayArg = function(argArray) {
+	say("there are " + argArray.length + " elements in my array.");
+}
+
 // Main Code
-readyLoop();
+var getReady = readyLoop();
 newFuction();
 forLoop();
 
@@ -82,8 +97,15 @@ var secondIncome = annualIncome(jsonData.playerNames[1].personFirstName,jsonData
 
 var thridIncome = annualIncome(jsonData.playerNames[2].personFirstName,jsonData.playerNames[2].hrIncome,jsonData.playerNames[2].overallTax);
 
+say("While money isn't everything, so isn't creativity. I can't think of anything right now to make a story but I can show you what I know");
 
+var array = Array2(2,"theman");
 
+arrayArg(array);
 
+// Outputs
+say("____________________");
 say(firstIncome);
-//*/
+say(getReady);
+say(array);
+
